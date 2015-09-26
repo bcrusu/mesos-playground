4253cd2cbe0d99a7815ea6414bc5191ed284905c
bcrusu <bcrusu@users.noreply.github.com>
2015-09-26 16:38:07 +0300

    added mesosdev image for running Mesos executors written in Scala

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 MESOS_CONTEXT="./mesos"
+MESOSDEV_CONTEXT="./mesosdev"
 
 docker build -t bcrusu/mesos:latest -f $MESOS_CONTEXT/dockerfile $MESOS_CONTEXT
+docker build -t bcrusu/mesosdev:latest -f $MESOSDEV_CONTEXT/dockerfile $MESOSDEV_CONTEXT
